bfb3ce5b395e356f3b9f5f20a8bb47629ed74461
bjxuss <bjacuna@unicesar.edu.co>
2024-02-23 21:31:15 -0500

    Pasando el componente ButtonHome a la carpeta 'Global'

---
+++ src/components/Global/ButtonHome.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
+const ButtonList = [
+  { name: "Interés Simple", url: "/is" },
+  { name: "Interés Compuesto", url: "/ic" },
+  { name: "Anualidad", url: "/an" },
+];
+export default function ButtonHome() {
+  const navigate = useNavigate();
+
+  const handleClick = (url: string) => {
+    navigate(url);
+  };
+  return (
+    <>
+      {ButtonList.map((button, index) => {
+        return (
+          <button className="text-black text-2xl mr-4 p-4 bg-slate-300 rounded-lg 
+                            hover:scale-105 hover:bg-slate-400 duration-200" 
+          onClick={() => handleClick(button.url)} key={index}>
+            {button.name}
+          </button>
+        );
+      })}
+    </>
+  );
+}
