c4c05750412b3b8cfbd38d2bbd0660f5ad9bc6cc
bjxuss <bjacuna@unicesar.edu.co>
2024-03-01 22:25:36 -0500

    Actualizando los campos de las props

--- src/components/Global/InputControl.tsx
+++ src/components/Global/InputControl.tsx
@@ -4,8 +4,10 @@ import React, { ChangeEvent } from "react"
 interface Props {
     labelName: string,
     inputName: string,
-    value?: number,
-    handleInputChange: (event: ChangeEvent<HTMLInputElement>) => void
+    value?: number | string,
+    type: string
+    handleInputChange: (event: ChangeEvent<HTMLInputElement>) => void,
+    disabled?: boolean
 
 }
 
@@ -15,10 +17,11 @@ const InputControl: React.FC<Props> = (props) => {
         <>
             
             <label htmlFor="" className="text-stone-900">{props.labelName}</label>
-                <input type="number" name={props.inputName} id=""
+                <input type={props.type} name={props.inputName} id=""
                     className="bg-slate-200 text-stone-900 px-3 py-2 rounded-lg block mb-2 w-full" 
                     onChange={e => props.handleInputChange(e)}
-                    value={props.value} />
+                    value={props.value} 
+                    disabled={props.disabled}    />
         </> 
     )
 }
