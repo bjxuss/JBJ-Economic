0ae55d625e54e431c80e2c6abf44b260d84dda6d
Juan David <misterio562@gmail.com>
2024-03-07 20:17:36 -0500

    modify IC

--- src/pages/InteresCompuesto/Form.tsx
+++ src/pages/InteresCompuesto/Form.tsx
@@ -1,60 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Form() {
-  const [result, setResult] = useState(Number);
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+  }, [result]); // Ejecutar el efecto cada vez que result cambie
+
 
   const calculateResult = (event: React.FormEvent) => {
     event.preventDefault();
-    // const montoCompuesto = parseFloat((document.getElementById('montocompuesto') as HTMLInputElement).value);
-    const capital = parseFloat(
-      (document.getElementById("capital") as HTMLInputElement).value
-    );
-    const tasaInteres = parseFloat(
-      (document.getElementById("tasainteres") as HTMLInputElement).value
-    );
-    // const anio = parseInt((document.getElementById('anio') as HTMLInputElement).value, 10);
-    const mes = parseInt(
-      (document.getElementById("mes") as HTMLInputElement).value,
-      10
-    );
-    // const dia = parseInt((document.getElementById('dia') as HTMLInputElement).value, 10);
+
+    const montoCompuestoInput = (
+      document.getElementById("montocompuesto") as HTMLInputElement
+    ).value;
+    const capitalInput = (
+      document.getElementById("capital") as HTMLInputElement
+    ).value;
+    const tasaInteresInput = (
+      document.getElementById("tasainteres") as HTMLInputElement
+    ).value;
+    const mesInput = (document.getElementById("mes") as HTMLInputElement).value;
+
+    
+    // Parsear los valores
+    const capital = parseFloat(capitalInput);
+    const tasaInteres = parseInt(tasaInteresInput, 10);
+    const mes = parseInt(mesInput, 10);
 
     // Convierte la tasa de interés a decimal
     const tasaDecimal = tasaInteres / 100;
 
     // Calcula el monto compuesto utilizando la fórmula
-    setResult(capital * Math.pow(1 + tasaDecimal, mes));
+    if (montoCompuestoInput === "") {
+      const resultMonto = capital * Math.pow(1 + tasaDecimal, mes);
+      setResult(resultMonto);
+      
+      const montocompuestoResult = document.getElementById("montocompuesto") as HTMLInputElement
+      montocompuestoResult.value = resultMonto.toFixed(2);
+    }
     console.log(result);
-    
-
-    
   };
 
   return (
     <form
-      className="flex flex-col gap-2 [&>input]:rounded-md"
+      className="flex flex-col gap-2 [&>input]:rounded-md [&>input]:p-2"
       onSubmit={calculateResult}
     >
-      <input
-        type="text"
-        name="montocompuesto"
-        id="montocompuesto"
-        placeholder="Monto Compuesto"
-      />
-      <input type="text" name="capital" id="capital" placeholder="Capital" />
-      <input
-        type="text"
-        name="tasainteres"
-        id="tasainteres"
-        placeholder="Tasa de Interés"
-      />
-      <input type="text" name="anio" id="anio" placeholder="Año" />
-      <input type="text" name="mes" id="mes" placeholder="Mes" />
-      <input type="text" name="dia" id="dia" placeholder="Dia" />
+      <input type="text" id="montocompuesto" placeholder="Monto Compuesto" />
+      <input type="text" id="capital" placeholder="Capital" />
+      <input type="text" id="tasainteres" placeholder="Tasa de Interés" />
+      <input type="text" id="anio" placeholder="Año" />
+      <input type="text" id="mes" placeholder="Mes" />
+      <input type="text" id="dia" placeholder="Dia" />
       <button type="submit">Calcular</button>
     </form>
-    
-    
-
   );
 }
--- src/pages/InteresCompuesto/InteresCompuesto.tsx
+++ src/pages/InteresCompuesto/InteresCompuesto.tsx
@@ -1,3 +1,4 @@
+import CardsForm from "../../components/Global/CardsForm";
 import Form from "./Form";
 
 export default function InteresCompuesto() {
@@ -6,11 +7,7 @@ export default function InteresCompuesto() {
       <div className="flex justify-center items-center text-2xl text-slate-200">
         InteresCompuesto
       </div>
-      <div className="flex justify-center items-center h-screen">
-        <div className="bg-white shadow-md shadow-slate-300 rounded-lg p-6">
-          <Form />
-        </div>
-      </div>
+      <CardsForm Component={Form}/>
     </>
   );
 }
